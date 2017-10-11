09a06c78b056a266643757c17a0d53ab3000625f
dagi12 <dagi12@o2.pl>
2017-10-11 17:14:05 +0200

    initial commit

---
+++ ErykIosCommon/Classes/StringHelper.swift
@@ -0,0 +1,29 @@
+//
+//  StringHelper.swift
+//  erent-ceo-ios
+//
+//  Created by Eryk Mariankowski on 11.10.2017.
+//  Copyright © 2017 Eryk Mariankowski. All rights reserved.
+//
+
+import Foundation
+
+class StringHelper {
+    
+    func randomString(length: Int) -> String {
+        
+        let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        let len = UInt32(letters.length)
+        
+        var randomString = ""
+        
+        for _ in 0 ..< length {
+            let rand = arc4random_uniform(len)
+            var nextChar = letters.character(at: Int(rand))
+            randomString += NSString(characters: &nextChar, length: 1) as String
+        }
+        
+        return randomString
+    }
+    
+}
