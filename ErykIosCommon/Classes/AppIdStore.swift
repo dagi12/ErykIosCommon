ae23fce661d59d4ed5dfacbaf4df3d114cfc5735
dagi12 <dagi12@o2.pl>
2017-10-18 10:47:35 +0200

    dłuższy app key

--- ErykIosCommon/Classes/AppIdStore.swift
+++ ErykIosCommon/Classes/AppIdStore.swift
@@ -9,13 +9,16 @@ import Foundation
 
 public class AppIdStore {
     
+    static let appKeyLength = 6
+    
     public static func getId(appKey: String) -> String {
         if let key = UserDefaults.standard.string(forKey: appKey) {
             return key;
         }
-        let newKey = StringHelper.randomString(length: 5);
+        let newKey = StringHelper.randomString(length: appKeyLength);
         UserDefaults.standard.set(newKey, forKey: appKey)
         return newKey
     }
     
 }
+
