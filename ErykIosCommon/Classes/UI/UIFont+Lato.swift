2e43999121777e2a9dc28d039a691c601ec4429a
dagi12 <dagi12@o2.pl>
2017-12-06 18:15:21 +0100

    final font fix

---
+++ ErykIosCommon/Classes/UI/UIFont+Lato.swift
@@ -0,0 +1,26 @@
+//
+//  UIFont+Lato.swift
+//  Alamofire
+//
+//  Created by Eryk Mariankowski on 06.12.2017.
+//
+
+import UIKit
+
+
+extension UIFont {
+    
+    class func loadFont(withName fontName: String) {
+        let bundleURL: URL? = Bundle(for: self).url(forResource: "ErykIosCommon",
+                                                    withExtension: "bundle")
+        let bundle = Bundle(url: bundleURL!)
+        let fontURL: URL? = bundle?.url(forResource: fontName,
+                                        withExtension: "ttf")
+        let fontData = try! Data(contentsOf: fontURL!)
+        let provider = CGDataProvider(data: fontData as CFData)!
+        if let font = CGFont(provider) {
+            CTFontManagerRegisterGraphicsFont(font, nil)
+        }
+    }
+    
+}
