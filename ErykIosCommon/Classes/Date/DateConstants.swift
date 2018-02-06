14c0f94b10ec84308d3dd97ddac556fd67df6f6d
dagi12 <dagi12@o2.pl>
2018-02-07 00:23:19 +0100

    #003738 - daty zaczynają się od Listopada, zamiast Stycznia

---
+++ ErykIosCommon/Classes/Date/DateConstants.swift
@@ -0,0 +1,14 @@
+//
+//  DateConstants.swift
+//  ErykIosCommon
+//
+//  Created by Eryk Mariankowski on 06.02.2018.
+//
+
+import Foundation
+
+public enum DateConstants {
+    public static let monthLength = 30
+    public static let twoWeeksLength = 14
+}
+
--- ErykIosCommon/Classes/Date/DateHelper.swift
+++ ErykIosCommon/Classes/Date/DateHelper.swift
@@ -9,17 +9,33 @@
 import Foundation
 
 public enum DateHelper {
-
-    public static func getCurrentMonth() -> Int {
-        let date = Date()
-        let calendar = Calendar.current
-        return calendar.component(.month, from: date)
+    
+    public static var dayOfMonth: Int {
+        get {
+            return Calendar.current.ordinality(of: .day, in: .month, for: Date())!
+        }
     }
-
-    public static func getCurrentDayOfYear() -> Int {
-        let date = Date()
-        let calendar = Calendar.current
-        return calendar.ordinality(of: .day, in: .year, for: date)!
+    
+    public static var currentYear: Int {
+        get {
+            return -1
+        }
+    }
+    
+    public static var currentMonth: Int {
+        get {
+            let date = Date()
+            let calendar = Calendar.current
+            return calendar.component(.month, from: date)
+        }
+    }
+    
+    public static var currentDayOfYear: Int {
+        get {
+            let date = Date()
+            let calendar = Calendar.current
+            return calendar.ordinality(of: .day, in: .year, for: date)!
+        }
     }
 
 }
