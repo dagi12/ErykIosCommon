4ededb355fed14cd708f4aeede228a5dc08756de
dagi12 <dagi12@o2.pl>
2018-02-08 17:56:35 +0100

    #003738 softra framework

---
+++ ErykIosCommon/Classes/DI/AppConstants.swift
@@ -0,0 +1,17 @@
+//
+//  AppConstants.swift
+//  erent-ceo-ios
+//
+//  Created by Eryk Mariankowski on 16.10.2017.
+//  Copyright © 2017 Eryk Mariankowski. All rights reserved.
+//
+
+import Foundation
+
+public protocol AppConstants {
+
+    var appVersion: String { get }
+    var appName: String { get }
+    var appKey: String { get }
+
+}
