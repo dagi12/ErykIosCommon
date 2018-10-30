3aad99ae085db1a6deed925ee4daa75bf5dcad39
Eryk Mariankowski <dagi12@o2.pl>
2018-10-30 11:25:25 +0100

    004306 końcowe poprawki (kolorowanie)

--- ErykIosCommon/Classes/Base/UIColor+init.swift
+++ ErykIosCommon/Classes/Base/UIColor+init.swift
@@ -12,5 +12,17 @@ extension UIColor {
         let newBlue = CGFloat(blue) / 255
         self.init(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
     }
+    
+    public convenience init(fromHexString hexString: String, withAlpha alpha: CGFloat = 1.0) {
+        var hexInt: UInt32 = 0
+        let scanner: Scanner = Scanner(string: hexString)
+        scanner.charactersToBeSkipped = CharacterSet(charactersIn: "#")
+        scanner.scanHexInt32(&hexInt)
+        let hexint = Int(hexInt)
+        let red = CGFloat((hexint & 0xff0000) >> 16) / 255.0
+        let green = CGFloat((hexint & 0xff00) >> 8) / 255.0
+        let blue = CGFloat((hexint & 0xff) >> 0) / 255.0
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
 
 }
