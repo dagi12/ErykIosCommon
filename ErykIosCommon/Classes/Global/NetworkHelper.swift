ec1323b5793da1222eaeed7494420bf21c131c60
Eryk Mariankowski <dagi12@o2.pl>
2019-01-08 15:06:24 +0100

    004321 skalowanie podpisów i nazwa zgodna z Android

--- ErykIosCommon/Classes/Global/NetworkHelper.swift
+++ ErykIosCommon/Classes/Global/NetworkHelper.swift
@@ -9,13 +9,17 @@ import Moya
 
 public enum NetworkHelper {
 
-    public static func attachmentToFormData(image: UIImage, title: String, description: String, group: String) -> [Moya.MultipartFormData] {
+    public static func attachmentToFormData(image: UIImage,
+                                            title: String,
+                                            description: String,
+                                            group: String,
+                                            fileName: String = "logo-square.jpg") -> [Moya.MultipartFormData] {
         let jpg = image.jpegData(compressionQuality: 0.8)!
         let title = MultipartFormData(provider: .data(title.data(using: .utf8)!), name: "tytul")
         let description = MultipartFormData(provider: .data(description.data(using: .utf8)!), name: "opis")
-        let square = MultipartFormData(provider: .data("logo-square.jpg".data(using: .utf8)!), name: "file_name")
+        let square = MultipartFormData(provider: .data(fileName.data(using: .utf8)!), name: "file_name")
         let grupaForm = MultipartFormData(provider: provider(group), name: "grupa")
-        let jpgData = MultipartFormData(provider: .data(jpg), name: "plik", fileName: "logo-square.jpg", mimeType: "image/jpg")
+        let jpgData = MultipartFormData(provider: .data(jpg), name: "plik", fileName: fileName, mimeType: "image/jpg")
         return [title, description, square, grupaForm, jpgData]
     }
 
