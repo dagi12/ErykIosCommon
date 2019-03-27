2c66c1284c2daf8890bf88f4688d189296bf89c7
Eryk Mariankowski <dagi12@o2.pl>
2019-03-27 13:58:33 +0100

    004686 wyświetlenie opcji, modele, stringi

---
+++ ErykIosCommon/Classes/UI/ButtonTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ButtonTableViewCell.swift
+//  ErykIosCommon
+//
+//  Created by Eryk Mariankowski on 27.03.2019.
+//
+
+import UIKit
+
+public class ButtonTableViewCell: UITableViewCell {
+
+    @IBOutlet public weak var button: UIButton!
+
+}
